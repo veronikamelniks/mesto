9f23a7624a2ae8cefb591474423b9d1d8839a6d7
veronikamelniks <126020773+veronikamelniks@users.noreply.github.com>
2023-06-08 12:23:02 +0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+const popUp = document.querySelector(".popup");
+const closePopupButton = document.querySelector(".popup__close");
+const openPopupButton = document.querySelector(".profile__edit-button");
+const popupMain = document.querySelector(".popup__text");
+
+
+let profileTitle = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+let popupInputTitle = popupMain.querySelector(".popup__input_input_title");
+let popupInputDescription = popupMain.querySelector(".popup__input_input_description");
+
+
+const openPopup = function () {
+    popUp.classList.add("popup_opened");
+    popupInputTitle.value = profileTitle.textContent;
+    popupInputDescription.value = profileDescription.textContent;
+};
+
+const closePopup = function () {
+    popUp.classList.remove("popup_opened");
+};
+
+openPopupButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = popupInputTitle.value;
+    profileDescription.textContent = popupInputDescription.value;
+    closePopup();
+};
+
+popUp.addEventListener("submit", handleFormSubmit);
